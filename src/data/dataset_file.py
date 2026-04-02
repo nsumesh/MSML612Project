d002f5311787902c9317e74dc48e94bacd846d96
Nikhil Sumesh <82290682+nsumesh@users.noreply.github.com>
2026-04-02 10:16:42 -0400

    dataset file updated errors

--- src/data/dataset_file.py
+++ src/data/dataset_file.py
@@ -5,29 +5,26 @@ from pathlib import Path
 
 class F1LapDataset(Dataset):
 
-    def __init__(self, data_dir: str ="data/splits/"):
+    def __init__(self, split: str, data_dir: str = "data/splits/"):
         data_dir = Path(data_dir)
-        numpy_data_path = data_dir/"data.npy"
-        labels_data_path = data_dir/"labels.npy"
-        self.data = torch.tensor(np.load(numpy_data_path), dtype = torch.float32)
-        self.labels = torch.tensor(np.load(labels_data_path), dtype = torch.float32) 
-        
-    def length(self):
+        self.data = torch.tensor(np.load(data_dir / f"data_{split}.npy"), dtype=torch.float32)
+        self.labels = torch.tensor(np.load(data_dir / f"labels_{split}.npy"), dtype=torch.float32)
+
+    def __len__(self):
         return len(self.data)
-    
-    def get_item(self, index):
+
+    def __getitem__(self, index):
         return self.data[index], self.labels[index]
 
-def get_dataloaders(data_dir : str = "data/splits/", batch_size : int = 64):
-    training_dataset = F1LapDataset("train", data_dir)
-    validation_dataset = F1LapDataset("validation", data_dir)
-    test_dataset = F1LapDataset("test", data_dir)
+def get_dataloaders(data_dir: str = "data/splits/", batch_size: int = 64):
+    training_dataset   = F1LapDataset("train", data_dir)
+    validation_dataset = F1LapDataset("val",   data_dir)
+    test_dataset       = F1LapDataset("test",  data_dir)
 
-    training_dataloader = DataLoader(training_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-    testing_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    training_dataloader   = DataLoader(training_dataset,   batch_size=batch_size, shuffle=True,  num_workers=2)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    testing_dataloader    = DataLoader(test_dataset,       batch_size=batch_size, shuffle=False, num_workers=2)
     return training_dataloader, validation_dataloader, testing_dataloader
 
-if __name__=="__main__":
+if __name__ == "__main__":
     training_dataloader, validation_dataloader, testing_dataloader = get_dataloaders(batch_size=64)
-    
