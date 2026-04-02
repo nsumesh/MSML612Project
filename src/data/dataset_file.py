99e87fc6f4501510e10bbd964941f55857482bd8
Nikhil Sumesh <82290682+nsumesh@users.noreply.github.com>
2026-04-02 10:18:33 -0400

    final

--- src/data/dataset_file.py
+++ src/data/dataset_file.py
@@ -10,10 +10,10 @@ class F1LapDataset(Dataset):
         self.data = torch.tensor(np.load(data_dir / f"data_{split}.npy"), dtype=torch.float32)
         self.labels = torch.tensor(np.load(data_dir / f"labels_{split}.npy"), dtype=torch.float32)
 
-    def __len__(self):
+    def length(self):
         return len(self.data)
 
-    def __getitem__(self, index):
+    def get_item(self, index):
         return self.data[index], self.labels[index]
 
 def get_dataloaders(data_dir: str = "data/splits/", batch_size: int = 64):
@@ -21,9 +21,9 @@ def get_dataloaders(data_dir: str = "data/splits/", batch_size: int = 64):
     validation_dataset = F1LapDataset("val",   data_dir)
     test_dataset       = F1LapDataset("test",  data_dir)
 
-    training_dataloader   = DataLoader(training_dataset,   batch_size=batch_size, shuffle=True,  num_workers=2)
+    training_dataloader = DataLoader(training_dataset,batch_size=batch_size, shuffle=True,  num_workers=2)
     validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
-    testing_dataloader    = DataLoader(test_dataset,       batch_size=batch_size, shuffle=False, num_workers=2)
+    testing_dataloader = DataLoader(test_dataset,batch_size=batch_size, shuffle=False, num_workers=2)
     return training_dataloader, validation_dataloader, testing_dataloader
 
 if __name__ == "__main__":
